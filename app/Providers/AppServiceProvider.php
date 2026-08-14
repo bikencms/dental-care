76abb7d5e4f50bae2fa7082301ed7766d62bdc95
Minh Pham <johndoe@example.com>
2026-08-14 11:31:26 +0700

    add https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator; // Import namespace này
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +23,10 @@ class AppServiceProvider extends ServiceProvider
     {
         // Đặt giao diện pagination mặc định toàn hệ thống
         Paginator::defaultView('auth.pagination.custom');
+
+        // Ép buộc toàn bộ route/URL sử dụng HTTPS khi chạy trên Production
+        if (config('app.env') === 'production' || request()->secure()) {
+            URL::forceScheme('https');
+        }
     }
 }
